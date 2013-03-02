13ecc35814f46c6811210fe75fe5f6735016d0f9
Kirill Korgov <korgov@yandex-team.ru>
2013-03-02 06:33:13 +0400

    meta-actions

--- src/java/ru/korgov/intellij/fugen/FuGenProjectComponent.java
+++ src/java/ru/korgov/intellij/fugen/FuGenProjectComponent.java
@@ -1,6 +1,10 @@
 package ru.korgov.intellij.fugen;
 
 
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.Anchor;
+import com.intellij.openapi.actionSystem.Constraints;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
@@ -8,6 +12,7 @@ import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import ru.korgov.intellij.fugen.actions.GenerateFieldFunctionsAction;
 import ru.korgov.intellij.fugen.properties.PersistentStateProperties;
 import ru.korgov.intellij.fugen.properties.ui.PropertiesWindow;
 
@@ -90,7 +95,21 @@ public class FuGenProjectComponent implements Configurable, ProjectComponent {
 
     @Override
     public void initComponent() {
+        final GenerateFieldFunctionsAction coded0 = new GenerateFieldFunctionsAction("Coded0", 0);
+        final GenerateFieldFunctionsAction coded1 = new GenerateFieldFunctionsAction("Coded1", 1);
 
+        final DefaultActionGroup group = new DefaultActionGroup();
+        group.addSeparator();
+        group.add(coded0);
+        group.add(coded1);
+        group.addSeparator();
+        final ActionManager actionManager = ActionManager.getInstance();
+        actionManager.registerAction("FuGenActionGroup", group);
+        actionManager.registerAction("FuGenActionCoded0", coded0);
+        actionManager.registerAction("FuGenActionCoded1", coded1);
+
+        final DefaultActionGroup genGroup = (DefaultActionGroup) actionManager.getAction("GenerateGroup");
+        genGroup.add(group, new Constraints(Anchor.AFTER, "JavaGenerateGroup2"));
     }
 
     @Override
